583b0317b5421904a83ceb2fe2dd568f04c73b86
HB <yanminch@gmail.com>
2020-07-29 12:44:30 -0400

    test commit

---
+++ LaSomme100/src/com/harut/test/SommeDemo.java
@@ -0,0 +1,26 @@
+package com.harut.test;
+
+public class SommeDemo {
+
+public static void main(String[] args) {
+int a=0;
+for(int i=0;i<=100;i++) {
+a=a+i;
+}
+
+System.out.println("La somme des 100 premiers nombre est " + a);
+
+System.out.print("Les nombre divisible par 5 sont ");
+for (int k=1; k<=100;k++)
+	if (k%5==0)
+
+System.out.print( k+ " ");
+System.out.println("");
+System.out.print("Les nombre divisible par 2 sont ");
+for (int k=1; k<=100;k++)
+	if (k%2==0) 
+
+System.out.print(k + " ");
+	}
+
+}
